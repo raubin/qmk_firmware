d266a02479cd080de3581f7e972c5a87bed7ef35
Robert Aubin <rob@thinkoomph.com>
2019-06-19 11:28:54 -0400

    Trying out a mod-tap for control key.

--- keyboards/planck/keymaps/rob/keymap.c
+++ keyboards/planck/keymaps/rob/keymap.c
@@ -47,6 +47,8 @@ enum planck_keycodes {
 #define RAISE MO(_RAISE)
 // Layer 3 when held, esc when tapped
 #define L3 LT(_LAYER_3, KC_ESC)
+// Control key when held, quote when tapped
+#define CTL_QT LCTL_T(KC_QUOT)
 
 // Browser forward and back macros (Chrome)
 #define BROWSEBACK LGUI(LSFT(KC_LBRACKET))
@@ -75,19 +77,19 @@ enum planck_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* Qwerty
- * ,-----------------------------------------------------------------------------------.
- * | Tab  |   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  | Bksp |
- * |------+------+------+------+------+-------------+------+------+------+------+------|
- * | L3   |   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   ;  |  "   |
- * |------+------+------+------+------+------|------+------+------+------+------+------|
- * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |Enter |
- * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |Browsr| Ctrl | Alt  | GUI  |Lower |    Space    |Raise |  SCRN  | Down | Up |Browsr|
- * `-----------------------------------------------------------------------------------'
+ * ,---------------------------------------------------------------------------------------.
+ * | Tab  |   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  |   Bksp   |
+ * |------+------+------+------+------+-------------+------+------+------+------+----------|
+ * | L3   |   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   ;  | LCTRL(') |
+ * |------+------+------+------+------+------|------+------+------+------+------+----------|
+ * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  | Enter    |
+ * |------+------+------+------+------+------+------+------+------+------+------+----------|
+ * |Browsr| Ctrl | Alt  | GUI  |Lower |    Space    |Raise |  SCRN  | Down | Up | Browsr   |
+ * `---------------------------------------------------------------------------------------'
  */
 [_QWERTY] = LAYOUT_planck_grid(
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,     KC_BSPC,
-    L3,      KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,  KC_QUOT,
+    L3,      KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,  CTL_QT,
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_ENT ,
     BROWSEBACK, KC_LCTL, KC_LALT, KC_LGUI, LOWER, KC_SPC, KC_SPC, RAISE,    CAP_SCRN,KC_DOWN, KC_UP,    BROWSEFORW
 ),
